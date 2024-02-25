7343d1802cc4f704fcfa0392c1d961763eba570c
Nadnerde <115773895+Clt689@users.noreply.github.com>
2024-02-25 23:26:03 +0900

    #2-자료형

---
+++ #2-자료형.js
@@ -0,0 +1,16 @@
+const name = "Mike";
+const age = 30;
+
+const name1 = "Mike";
+const name2 = 'Mike';
+const name3 = `Mike`;
+
+const message = "I'm a boy. ";
+const message2 = 'I\'m a boy. ';
+const message3 = `My name is ${name}`;
+const message4 = `나는 ${age+1}살 입니다.`;
+console.log(message4);
+
+// 자바스크립트에서 변수를 선언할 때는,
+// 변하지 않는 값은 const,
+// 변할 수 있는 값은 let 으로 선언하세요.
