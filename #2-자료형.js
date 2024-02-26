581b20f76d276fea2bdb3a67741e02ec63540972
Nadnerde <115773895+Clt689@users.noreply.github.com>
2024-02-26 11:44:23 +0900

    자바스크립트 폴더 및 파일 정리

--- #2-자료형.js
+++ #2-자료형.js
@@ -10,7 +10,3 @@ const message2 = 'I\'m a boy. ';
 const message3 = `My name is ${name}`;
 const message4 = `나는 ${age+1}살 입니다.`;
 console.log(message4);
-
-// 자바스크립트에서 변수를 선언할 때는,
-// 변하지 않는 값은 const,
-// 변할 수 있는 값은 let 으로 선언하세요.
