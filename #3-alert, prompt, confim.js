5ddbfe045ba2a5fece2232190befc1cdb2f19015
Nadnerde <115773895+Clt689@users.noreply.github.com>
2024-02-27 21:44:10 +0900

    ~6장 파일 추가

---
+++ #3-alert, prompt, confim.js
@@ -0,0 +1,12 @@
+const name = "Mike";
+const age = 30;
+
+const name1 = "Mike";
+const name2 = 'Mike';
+const name3 = `Mike`;
+
+const message = "I'm a boy. ";
+const message2 = 'I\'m a boy. ';
+const message3 = `My name is ${name}`;
+const message4 = `나는 ${age+1}살 입니다.`;
+console.log(message4);
