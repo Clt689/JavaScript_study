295def3ac6d7fe6235c541d8eb44323e18cd2cfd
Nadnerde <115773895+Clt689@users.noreply.github.com>
2024-05-07 17:07:20 +0900

    11-화살표 함수

---
+++ 자바스크립트 기초/#11/#11-함수 표현식, 화살표 함수.js
@@ -0,0 +1,11 @@
+// 화살표 함수
+
+// let sayHello = (name) => {
+//   const msg = `Hello, ${name}`;
+//   console.log(msg);
+// }
+
+// sayHello('Lee');
+
+const add = (num1, num2) => num1 + num2;
+add();
